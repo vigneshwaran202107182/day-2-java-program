791f6ba44765fbcea64172350d9ecdba07587e4d
vigneshwaran202107182 <117887936+vigneshwaran202107182@users.noreply.github.com>
2023-07-11 21:06:20 +0530

    Create counting odd or even number.java

---
+++ counting odd or even number.java
@@ -0,0 +1,40 @@
+int count=0;
+        if(input6.equalsIgnoreCase("odd")){
+            if(input1%2!=0){
+                count+=1;
+            }
+             if(input2%2!=0){
+                count+=1;
+            }
+             if(input3%2!=0){
+                count+=1;
+            }
+             if(input4%2!=0){
+                count+=1;
+            }
+             if(input5%2!=0){
+                count+=1;
+            }
+           
+        }
+        else {
+            if(input1%2==0){
+                count+=1;
+            }
+             if(input2%2==0){
+                count+=1;
+            }
+             if(input3%2==0){
+                count+=1;
+            }
+             if(input4%2==0){
+                count+=1;
+            }
+             if(input5%2==0){
+                count+=1;
+            }
+            
+        }
+		return count;
+	}
+}
