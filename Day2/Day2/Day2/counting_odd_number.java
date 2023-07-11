ab86db4ed792cff9adb5ae8d1c11dd0e2353f054
vigneshwaran202107182 <117887936+vigneshwaran202107182@users.noreply.github.com>
2023-07-11 20:54:57 +0530

    Create counting_odd_number.java

---
+++ Day2/Day2/Day2/counting_odd_number.java
@@ -0,0 +1,20 @@
+ int count=0;
+        if(input1%2!=0){
+        count+=1;
+    }
+        if(input2%2!=0){
+        count+=1;
+    }
+        if(input3%2!=0){
+        count+=1;
+    }
+        if(input4%2!=0){
+        count+=1;
+    }
+        if(input5%2!=0){
+        count+=1;
+    }
+        
+		return count;
+	}
+}
