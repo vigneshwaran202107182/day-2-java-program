ce170508ce3ba1c92ba8a00a62a469033df1a525
vigneshwaran202107182 <117887936+vigneshwaran202107182@users.noreply.github.com>
2023-07-12 11:36:19 +0530

    Update counting odd or even number.java

--- counting odd or even number.java
+++ counting odd or even number.java
@@ -1,3 +1,9 @@
+import java.util.*;
+class oddEven{
+
+
+public int isoddeven(int input1, input2,input3,input4,input5,String input6){
+
 int count=0;
         if(input6.equalsIgnoreCase("odd")){
             if(input1%2!=0){
